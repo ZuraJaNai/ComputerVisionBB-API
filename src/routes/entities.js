cc83a19a9f5c9792a86e7030bd52be2a9c1fe6c7
Oleksiy Yevtushok <alexey.yevtushok@gmail.com>
2019-04-22 17:17:09 +0300

    editEntity & delImage

--- src/routes/entities.js
+++ src/routes/entities.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const fs = require("fs");
-const konva = require("konva");
+const fs = require('fs');
+const konva = require('konva');
 // let entities = require("../data/entities");
 
 // @route GET api/entities
 // @desc get list of all entities
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   let entitiesJSON = JSON.parse(
-    fs.readFileSync(__dirname + "/../data/entities.json")
+    fs.readFileSync(__dirname + '/../data/entities.json'),
   );
   res.status(200).json(entitiesJSON);
 });
 
 // @route POST api/entities
 // @desc   new entity
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   let entitiesJSON = JSON.parse(
-    fs.readFileSync(__dirname + "/../data/entities.json")
+    fs.readFileSync(__dirname + '/../data/entities.json'),
   );
   for (let i = 0; i < entitiesJSON.length; i++) {
     if (entitiesJSON[i].color === req.body.color) {
@@ -26,19 +26,19 @@ router.post("/", (req, res) => {
   }
   const entity = {
     index: entitiesJSON.length,
-    ...req.body
+    ...req.body,
   };
   entitiesJSON.push(entity);
   let data = JSON.stringify(entitiesJSON);
-  fs.writeFileSync("./src/data/entities.json", data);
+  fs.writeFileSync('./src/data/entities.json', data);
   res.status(201).json(entity);
 });
 
-router.delete("/:index", (req, res) => {
+router.delete('/:index', (req, res) => {
   let data;
   let newEntitesJSON = [];
   let entitiesJSON = JSON.parse(
-    fs.readFileSync(__dirname + "/../data/entities.json")
+    fs.readFileSync(__dirname + '/../data/entities.json'),
   );
   for (let i = 0; i < entitiesJSON.length; i++) {
     if (entitiesJSON[i].index == req.params.index) {
@@ -47,13 +47,23 @@ router.delete("/:index", (req, res) => {
       let newEntity = {
         index: newEntitesJSON.length,
         color: entitiesJSON[i].color,
-        label: entitiesJSON[i].label
+        label: entitiesJSON[i].label,
       };
       newEntitesJSON.push(newEntity);
     }
   }
   data = JSON.stringify(newEntitesJSON);
-  fs.writeFileSync("./src/data/entities.json", data);
+  fs.writeFileSync('./src/data/entities.json', data);
+  res.status(200).json(data);
+});
+
+router.put('/', (req, res) => {
+  let entitiesJSON = JSON.parse(
+    fs.readFileSync(__dirname + '/../data/entities.json'),
+  );
+  entitiesJSON[req.body.index].label = req.body.data;
+  let data = JSON.stringify(entitiesJSON);
+  fs.writeFileSync('./src/data/entities.json', data);
   res.status(200).json(data);
 });
 
--- src/routes/images.js
+++ src/routes/images.js
@@ -1,42 +1,42 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const fs = require("fs");
-const hound = require("hound");
-const watcher = hound.watch("./img/");
-let images = require("../data/images");
-const multer = require("multer");
+const fs = require('fs');
+const hound = require('hound');
+const watcher = hound.watch('./img/');
+let images = require('../data/images');
+const multer = require('multer');
 
 /*modify json on folder change*/
-watcher.on("create", () => {
+watcher.on('create', () => {
   folderOnChange();
 });
-watcher.on("delete", () => {
+watcher.on('delete', () => {
   folderOnChange();
 });
 
 const folderOnChange = () => {
-  fs.readdir("./img/", (err, items) => {
+  fs.readdir('./img/', (err, items) => {
     /*sort by date*/
     items = items.sort(
       (a, b) =>
-        fs.statSync("./img/" + b).mtime.getTime() -
-        fs.statSync("./img/" + a).mtime.getTime()
+        fs.statSync('./img/' + b).mtime.getTime() -
+        fs.statSync('./img/' + a).mtime.getTime(),
     );
     let imagesEmpty = [];
     /*if 0 img -> empty JSON*/
     if (items.length === 0) {
       let data = JSON.stringify(imagesEmpty);
-      fs.writeFileSync("./src/data/images.json", data);
+      fs.writeFileSync('./src/data/images.json', data);
     } else {
       for (let i = 0; i < items.length; i++) {
         let image = {
           _id: imagesEmpty.length,
           index: imagesEmpty.length,
-          picture: "http://localhost:5000/img/" + items[i]
+          picture: 'http://localhost:5000/img/' + items[i],
         };
         imagesEmpty.push(image);
         let data = JSON.stringify(imagesEmpty);
-        fs.writeFileSync("./src/data/images.json", data);
+        fs.writeFileSync('./src/data/images.json', data);
       }
     }
   });
@@ -45,37 +45,43 @@ const folderOnChange = () => {
 /*store data in this folder*/
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./img/");
+    cb(null, './img/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "." + file.originalname);
-  }
+    cb(null, Date.now() + '.' + file.originalname);
+  },
 });
 // filter by file type
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/png"
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/png'
   ) {
     cb(null, true);
   } else {
-    cb(new Error("incorrect file format"), false);
+    cb(new Error('incorrect file format'), false);
   }
 };
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 /*request*/
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   let imagesJSON = JSON.parse(
-    fs.readFileSync(__dirname + "/../data/images.json")
+    fs.readFileSync(__dirname + '/../data/images.json'),
   );
   res.status(200).json(imagesJSON);
 });
 
-router.post("/", upload.single("targetImage"), (req, res) => {
+router.post('/', upload.single('targetImage'), (req, res) => {
   res.status(201).json(images);
+});
+
+router.delete('/:img', (req, res) => {
+  let path = req.params.img;
+  fs.unlinkSync('./img/' + path);
+  res.status(201);
 });
 
 module.exports = router;
