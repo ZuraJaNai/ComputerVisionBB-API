16bdbe2e467af6c687723fa7008dd1d78ba58951
Mykola Kovtun <nckkvtn@gmail.com>
2019-04-23 14:46:16 +0300

    refactored code

--- src/routes/generator.js
+++ src/routes/generator.js
@@ -3,59 +3,78 @@ const router = express.Router();
 const builder = require("xmlbuilder");
 const archiver = require("archiver");
 const fs = require("fs");
+const path = require("path");
 
 router.get("/", (req, res) => {
-  const images = JSON.parse(fs.readFileSync("./src/data/labeledImages.json"));
-  images.forEach(element => {
-    const image = element.image;
-    const labeledShapes = element.shapes;
-    var xml = builder
-      .create("annotation")
-      .ele("filename", image.name)
-      .up()
-      .ele("size")
-      .ele("width", image.width)
-      .up()
-      .ele("height", image.height)
-      .up()
-      .up();
-    labeledShapes.forEach(shape => {
-      xml
-        .ele("object")
-        .ele("name", shape.label)
-        .up()
-        .ele("bndbox")
-        .ele("xmin", shape.x)
-        .up()
-        .ele("ymin", shape.y)
-        .up()
-        .ele("xmax", shape.x + shape.width)
-        .up()
-        .ele("ymax", shape.y + shape.height)
-        .up()
-        .up()
-        .up();
-    });
-    data = xml.end({ pretty: true });
-    fs.writeFileSync(`./output/${image.name}.xml`, data);
+  const directory = "./output";
+  const labeledImages = JSON.parse(
+    fs.readFileSync("./src/data/labeledImages.json")
+  );
+  labeledImages.forEach(element => {
+    createXML(element.image, element.shapes, directory);
   });
-  //create archive and send it
-  //add pictures which are labeled to archive ???
   res.setHeader("Content-Type", "application/zip");
-  pipeZipToRes(req, res);
-  //delete xml files
+  pipeZipToRes(req, res, directory);
 });
 
-function pipeZipToRes(req, res) {
+function createXML(image, shapes, directory) {
+  var xml = builder
+    .create("annotation")
+    .ele("filename", image.name)
+    .up()
+    .ele("size")
+    .ele("width", image.width)
+    .up()
+    .ele("height", image.height)
+    .up()
+    .up();
+  shapes.forEach(shape => {
+    xml
+      .ele("object")
+      .ele("name", shape.label)
+      .up()
+      .ele("bndbox")
+      .ele("xmin", shape.x)
+      .up()
+      .ele("ymin", shape.y)
+      .up()
+      .ele("xmax", shape.x + shape.width)
+      .up()
+      .ele("ymax", shape.y + shape.height)
+      .up()
+      .up()
+      .up();
+  });
+  data = xml.end({ pretty: true });
+  fs.writeFileSync(`${directory}/${image.name}.xml`, data);
+}
+
+function pipeZipToRes(req, res, directory) {
   const zipfile = archiver("zip");
 
   zipfile.on("error", err => {
     throw err;
   });
 
+  zipfile.on("end", function() {
+    deleteFilesInDir(directory);
+  });
+
   zipfile.pipe(res);
 
-  zipfile.directory("./output", false).finalize();
+  zipfile.directory(directory, false).finalize();
+}
+
+function deleteFilesInDir(directory) {
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
 }
 
 module.exports = router;
