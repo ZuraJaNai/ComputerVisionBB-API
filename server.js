5975bf44881dae3fa108d8a8bef336ab69c7d696
Mykola Kovtun <nckkvtn@gmail.com>
2019-04-23 08:47:57 +0300

    added generator endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var entities = require("./src/routes/entities");
 var images = require("./src/routes/images");
+var generator = require("./src/routes/generator");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 
@@ -19,5 +20,6 @@ app.use("/img/", express.static("img"));
 //Routes
 app.use("/api/entities", entities);
 app.use("/api/images/", images);
+app.use("/api/generator/", generator);
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
