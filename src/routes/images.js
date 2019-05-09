2a5869bd0774222cf980435dbc3b48a5aa2e9b16
Mykola Kovtun <nckkvtn@gmail.com>
2019-05-09 17:26:53 +0300

    basic flow for downloading gdrive images

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,42 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const hound = require('hound');
-const watcher = hound.watch('./img/');
-let images = require('../data/images');
-const multer = require('multer');
+const fs = require("fs");
+const path = require("path");
+const hound = require("hound");
+const watcher = hound.watch("./img/");
+const axios = require("axios");
+let images = require("../data/images");
+const multer = require("multer");
 
 // Folder 'on change' listeners.
-watcher.on('create', () => {
+watcher.on("create", () => {
   folderOnChange();
 });
-watcher.on('delete', () => {
+watcher.on("delete", () => {
   folderOnChange();
 });
 // Do some actions if folder change.
 const folderOnChange = () => {
-  fs.readdir('./img/', (err, items) => {
+  fs.readdir("./img/", (err, items) => {
     // sort by date
     items = items.sort(
       (a, b) =>
-        fs.statSync('./img/' + b).mtime.getTime() -
-        fs.statSync('./img/' + a).mtime.getTime(),
+        fs.statSync("./img/" + b).mtime.getTime() -
+        fs.statSync("./img/" + a).mtime.getTime()
     );
     let imagesEmpty = [];
     // if 0 img -> empty JSON
     if (items.length === 0) {
       let data = JSON.stringify(imagesEmpty);
-      fs.writeFileSync('./src/data/images.json', data);
+      fs.writeFileSync("./src/data/images.json", data);
     } else {
       for (let i = 0; i < items.length; i++) {
         let image = {
           _id: imagesEmpty.length,
           index: imagesEmpty.length,
-          picture: 'img/' + items[i],
+          picture: "img/" + items[i],
         };
         imagesEmpty.push(image);
         let data = JSON.stringify(imagesEmpty);
-        fs.writeFileSync('./src/data/images.json', data);
+        fs.writeFileSync("./src/data/images.json", data);
       }
     }
   });
@@ -45,40 +47,71 @@ const folderOnChange = () => {
 // Choose file for data storage.
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './img/');
+    cb(null, "./img/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '.' + file.originalname);
+    cb(null, Date.now() + "." + file.originalname);
   },
 });
 // Addition file filter by file type.
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/png'
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
   ) {
     cb(null, true);
   } else {
-    cb(new Error('incorrect file format'), false);
+    cb(new Error("incorrect file format"), false);
   }
 };
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 const deleteImageShapes = name => {
-  const images = JSON.parse(fs.readFileSync('./src/data/labeledImages.json'));
+  const images = JSON.parse(fs.readFileSync("./src/data/labeledImages.json"));
   const index = images.findIndex(obj => obj.image.name === name);
   if (index >= 0) {
     images.splice(index, 1);
-    fs.writeFileSync('./src/data/labeledImages.json', JSON.stringify(images));
+    fs.writeFileSync("./src/data/labeledImages.json", JSON.stringify(images));
   }
 };
 
+const downloadGDriveImages = (folderId, accessToken) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  const baseUrl = "https://www.googleapis.com/drive/v3/files";
+  const url = `${baseUrl}?q=parents%20in%20'${folderId}'`;
+  axios.get(url, { headers }).then(res => {
+    const files = res.data.files;
+    files.forEach(file => {
+      console.log(file);
+      // add of mimeType file -> download, else (mimeType folder) -> step into folder (recursion)
+      // add alt media to params
+      const imgPath = path.resolve("img", file.name);
+      const writer = fs.createWriteStream(imgPath);
+      axios
+        .get(`${baseUrl}/${file.id}`, {
+          headers,
+          params: { alt: "media" },
+          responseType: "stream",
+        })
+        .then(res => {
+          res.data.pipe(writer);
+        });
+    });
+  });
+};
+
+const getFolderId = url => {
+  // get folder id from url
+  return "1IWD3_xbGQv23892IJ0mzIoCQwjCaz9V3";
+};
 // @route GET api/images
 // @desc   get list of all images
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   let imagesJSON = JSON.parse(
-    fs.readFileSync(__dirname + '/../data/images.json'),
+    fs.readFileSync(__dirname + "/../data/images.json")
   );
   res
     .status(200)
@@ -88,7 +121,19 @@ router.get('/', (req, res) => {
 
 // @route POST api/images
 // @desc   new image
-router.post('/', upload.single('targetImage'), (req, res) => {
+router.post("/", upload.single("targetImage"), (req, res) => {
+  res
+    .status(201)
+    .json(images)
+    .end();
+});
+
+// @route POST api/images
+// @desc   new images from google drive
+router.post("/gdrive", (req, res) => {
+  const { url, accessToken } = req.body;
+  const folderId = getFolderId(url);
+  downloadGDriveImages(folderId, accessToken);
   res
     .status(201)
     .json(images)
@@ -97,33 +142,33 @@ router.post('/', upload.single('targetImage'), (req, res) => {
 
 // @route DELETE api/images
 // @desc   delete image
-router.delete('/img/:imgName', (req, res) => {
+router.delete("/img/:imgName", (req, res) => {
   let path = req.params.imgName;
-  if (fs.existsSync('./img/' + path)) {
+  if (fs.existsSync("./img/" + path)) {
     deleteImageShapes(path);
-    fs.unlinkSync('./img/' + path);
+    fs.unlinkSync("./img/" + path);
     res.status(200).send();
   } else {
-    console.log('no file');
+    console.log("no file");
   }
 });
 
 // @route POST api/images
 // @desc   reset all
 
-router.delete('/reset', (req, res) => {
+router.delete("/reset", (req, res) => {
   // reset entities
-  fs.writeFileSync('./src/data/entities.json', JSON.stringify([]));
+  fs.writeFileSync("./src/data/entities.json", JSON.stringify([]));
   // reset shapes
-  fs.writeFileSync('./src/data/labeledImages.json', JSON.stringify([]));
+  fs.writeFileSync("./src/data/labeledImages.json", JSON.stringify([]));
   // delete images
-  fs.readdir('./img/', (err, items) => {
+  fs.readdir("./img/", (err, items) => {
     for (let i = 0; i < items.length; i++) {
-      if (fs.existsSync('./img/' + items[i])) {
+      if (fs.existsSync("./img/" + items[i])) {
         deleteImageShapes(items[i]);
-        fs.unlinkSync('./img/' + items[i]);
+        fs.unlinkSync("./img/" + items[i]);
       } else {
-        console.log('no file');
+        console.log("no file");
       }
     }
   });
