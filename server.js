bf2d0c57a9d555db37e15e12f9c66101fe20bc93
Mykola Kovtun <nckkvtn@gmail.com>
2019-05-22 10:08:14 +0300

    changed port from 5000 to 8080

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const entities = require('./src/routes/entities');
-const images = require('./src/routes/images');
-const generator = require('./src/routes/generator');
-const labeledImages = require('./src/routes/labeledImages');
+const express = require("express");
+const bodyParser = require("body-parser");
+const entities = require("./src/routes/entities");
+const images = require("./src/routes/images");
+const generator = require("./src/routes/generator");
+const labeledImages = require("./src/routes/labeledImages");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false,
-  }),
+  })
 );
 
 app.use(bodyParser.json());
 // make folder public
-app.use('/img/', express.static('img'));
-app.use('/public/', express.static('public'));
+app.use("/img/", express.static("img"));
+app.use("/public/", express.static("public"));
 
 // Routes
-app.use('/api/entities', entities);
-app.use('/api/images/', images);
-app.use('/api/labeled/', labeledImages);
-app.use('/api/generator/', generator);
+app.use("/api/entities", entities);
+app.use("/api/images/", images);
+app.use("/api/labeled/", labeledImages);
+app.use("/api/generator/", generator);
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
